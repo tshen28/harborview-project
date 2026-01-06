ff1576c65a0e32bff72fbd8a4ec3b538c4102403
Taylor Shen <101141057+tshen28@users.noreply.github.com>
2026-01-06 13:59:47 -0800

    Adding starting files

---
+++ app/index.tsx
@@ -0,0 +1,10 @@
+//route: "/"
+import { Text, View } from 'react-native';
+
+export default function Index() {
+    return (
+        <View>
+            <Text>Healthcare Simulation App</Text>
+        </View>
+    );
+}
