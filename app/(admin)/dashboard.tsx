5c58cd13ffec4de38aa1807a8bf0dddd5a9f835b
Taylor Shen <101141057+tshen28@users.noreply.github.com>
2026-01-07 12:54:11 -0800

    feat: adding admin/student pathways

--- app/(admin)/dashboard.tsx
+++ app/(admin)/dashboard.tsx
@@ -1,1 +1,15 @@
 //route: "/admin/dashboard"
+import { useAuth } from "@/src/context/AuthContext";
+import { Redirect, Slot } from "expo-router";
+
+export default function AdminLayout() {
+    const { user, role, loading } = useAuth();
+
+    if (loading) return null;
+
+    if (!user || role !== 'admin') {
+        return <Redirect href="/" />;
+    }
+
+    return <Slot />;
+}
--- app/index.tsx
+++ app/index.tsx
@@ -1,32 +1,39 @@
 //route: "/"
-import { useAuth } from '@/src/context/AuthContext';
-import { Redirect } from 'expo-router';
-import { StyleSheet, Text, View } from 'react-native';
+import { useAuth } from "@/src/context/AuthContext";
+import { Redirect } from "expo-router";
+import { StyleSheet, Text } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Index() {
-    const { user, loading } = useAuth();
+  const { user, role, loading } = useAuth();
 
-    if (loading) return null;
-
-    if (!user) {
-        return <Redirect href="/login" />;
-    }
-
+  if (loading) {
     return (
-        <View style={styles.container}>
-            <Text style={styles.text}>Healthcare Simulation App</Text>
-        </View>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text}>Healthcare Simulation App</Text>
+      </SafeAreaView>
     );
+  }
+
+  if (!user) {
+    return <Redirect href="/(auth)/login" />;
+  }
+
+  if (role === 'admin') {
+    return <Redirect href="/(admin)/dashboard" />;
+  }
+
+  return <Redirect href="/(student)/dashboard" />;
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: 'white',
-        alignItems: 'center',
-        justifyContent: 'center',
-    },
-    text: {
-        color: 'black',
-    },
-})
+  container: {
+    flex: 1,
+    backgroundColor: "white",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  text: {
+    color: "black",
+  },
+});
